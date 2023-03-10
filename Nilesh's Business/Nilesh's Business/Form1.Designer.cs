fceca2b916da3b60b8a6e3c377b3dfb1a9e8d2fa
lucasziiN <127479088+lucasziiN@users.noreply.github.com>
2023-03-10 20:47:18 +1300

    Add files via upload

---
+++ Nilesh's Business/Nilesh's Business/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Nilesh_s_Business
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConcrete = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.drivewayLength = new System.Windows.Forms.TextBox();
+            this.drivewayWidth = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cementRequired = new System.Windows.Forms.TextBox();
+            this.concreteRequired = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonConcrete
+            // 
+            this.buttonConcrete.Location = new System.Drawing.Point(329, 106);
+            this.buttonConcrete.Name = "buttonConcrete";
+            this.buttonConcrete.Size = new System.Drawing.Size(132, 23);
+            this.buttonConcrete.TabIndex = 0;
+            this.buttonConcrete.Text = "Calculate Concrete Cost";
+            this.buttonConcrete.UseVisualStyleBackColor = true;
+            this.buttonConcrete.Click += new System.EventHandler(this.buttonConcrete_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(488, 386);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(132, 23);
+            this.buttonClear.TabIndex = 1;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(206, 386);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(132, 23);
+            this.buttonExit.TabIndex = 2;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Driveway Length";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(412, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Driveway Width";
+            // 
+            // drivewayLength
+            // 
+            this.drivewayLength.Location = new System.Drawing.Point(228, 62);
+            this.drivewayLength.Name = "drivewayLength";
+            this.drivewayLength.Size = new System.Drawing.Size(132, 20);
+            this.drivewayLength.TabIndex = 6;
+            // 
+            // drivewayWidth
+            // 
+            this.drivewayWidth.Location = new System.Drawing.Point(583, 62);
+            this.drivewayWidth.Name = "drivewayWidth";
+            this.drivewayWidth.Size = new System.Drawing.Size(132, 20);
+            this.drivewayWidth.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(224, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(261, 24);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Volume of Concrete Required";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(218, 283);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(267, 24);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Kilograms of Cement Required";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // cementRequired
+            // 
+            this.cementRequired.Location = new System.Drawing.Point(512, 288);
+            this.cementRequired.Name = "cementRequired";
+            this.cementRequired.Size = new System.Drawing.Size(80, 20);
+            this.cementRequired.TabIndex = 10;
+            this.cementRequired.TextChanged += new System.EventHandler(this.cementRequired_TextChanged);
+            // 
+            // concreteRequired
+            // 
+            this.concreteRequired.Location = new System.Drawing.Point(512, 176);
+            this.concreteRequired.Name = "concreteRequired";
+            this.concreteRequired.Size = new System.Drawing.Size(80, 20);
+            this.concreteRequired.TabIndex = 11;
+            this.concreteRequired.TextChanged += new System.EventHandler(this.concreteRequired_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(512, 216);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(80, 20);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(224, 216);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(237, 24);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Bags of Concrete Required";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(512, 332);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(80, 20);
+            this.textBox2.TabIndex = 15;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(218, 327);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(184, 24);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Total Cost of Cement";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.concreteRequired);
+            this.Controls.Add(this.cementRequired);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.drivewayWidth);
+            this.Controls.Add(this.drivewayLength);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonConcrete);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConcrete;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox drivewayLength;
+        private System.Windows.Forms.TextBox drivewayWidth;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox cementRequired;
+        private System.Windows.Forms.TextBox concreteRequired;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
